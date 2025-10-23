5d26fd47c602e4d8bd2eba558505fe2e57a78f48
Mauricio Lemos <mauriciomenezeslemos@gmail.com>
2025-10-23 14:55:49 -0300

    Document: Add a classe de estudo da aula 2.

---
+++ src/introducao/Aula01OlaDevDojo.java
@@ -0,0 +1,7 @@
+package introducao;
+
+public class Aula01OlaDevDojo {
+    static void main (){
+        System.out.println("Hello World");
+    }
+}
---
+++ src/introducao/Aula02TiposPrimitivos.java
@@ -0,0 +1,15 @@
+package introducao;
+
+public class Aula02TiposPrimitivos {
+    static void main() {
+        int numeroInteiro = 1;
+        double numeroQuebradoGrande=1.5;
+        float numeroGrande= 1.3F;
+        byte numerinhoZinho = 127;
+        char letrinha = 'a';
+        short numerinho= 32767;
+        long numeroInteiroGrande = 10000000000000L;
+        boolean falsoOuVerdadeiro = false;
+        falsoOuVerdadeiro=true;
+    }
+}
